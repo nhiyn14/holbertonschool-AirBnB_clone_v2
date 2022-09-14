bfecef45bce186627932829cde4048a7eb1a3e39
Cienna Nguyen <4326@holbertonschool.com>
2022-09-14 22:09:53 +1000

    Add task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+from 1-hbnb_route.py plus:
+/c/<text>: display “C ” followed by the value of text
+replace underscore (in text) with a space
+"""
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Display Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Display HBNB!"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """Display "C" + <text>"""
+    return f"C {text.replace('_', ' ')}"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
