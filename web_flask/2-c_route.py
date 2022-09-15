80c75342f41db37853d308469840c7ad02e93be5
Cienna Nguyen <4326@holbertonschool.com>
2022-09-15 10:28:54 +1000

    Remove fstring format

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -26,7 +26,7 @@ def hbnb():
 @app.route("/c/<text>", strict_slashes=False)
 def c_text(text):
     """Display C + text"""
-    return f"C {text.replace('_', ' ')}"
+    return "C " + text.replace('_', ' ')
 
 
 if __name__ == "__main__":
